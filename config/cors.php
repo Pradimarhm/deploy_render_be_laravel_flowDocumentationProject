760ed41821a86a7fc652c3360500bc2460cba1fa
Praditya <158058389+Pradimarhm@users.noreply.github.com>
2026-07-29 15:35:45 +0700

    ubah cors 2

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://fe-flow-documentation-website.onrender.com/auth'],
+    'allowed_origins' => ['https://fe-flow-documentation-website.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
