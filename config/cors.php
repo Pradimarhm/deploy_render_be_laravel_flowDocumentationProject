911277b87b10ce2f69fb091d3b02f3ee66f1064d
Praditya <158058389+Pradimarhm@users.noreply.github.com>
2026-07-30 23:57:11 +0700

    update cors izinkan local url sementara

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://fe-flow-documentation-website.onrender.com'],
+    'allowed_origins' => ['https://fe-flow-documentation-website.onrender.com', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
