bb1ffc9b2903a1657bbb192be52036818156466e
Praditya <158058389+Pradimarhm@users.noreply.github.com>
2026-07-31 08:56:28 +0700

    update cors izinkan local url sementara 2

--- app/Models/FlowNode.php
+++ app/Models/FlowNode.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class FlowNode extends Model
 {
+    use HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
     protected $fillable = [
         'template_id',
         'label',
